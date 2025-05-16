c2f6df5a73f3011ca6635067abe13b270c31b399
Ksawlii <ksawery.blaszczak@proton.me>
2025-05-16 21:12:06 +0200

    a53x: customize: fix adding missing tee blobs to selinux context files
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- customize.sh
+++ customize.sh
@@ -1,5 +1,10 @@
 set -e
 
+if [ "$TARGET_CODENAME" != "a53x" ]; then
+    echo "INFO: Target codename is not a53x, skipping vendor patch"
+    exit 0
+fi
+
 if [ "$SRC_DIR" ] && [ "$SRC_DIR/unica" ]; then
   UNICA="1"
   SKIPUNZIP=1
@@ -57,7 +62,7 @@ if [ "$UNICA" = "1" ]; then
     echo "on early-fs && property:ro.boot.em.model=SM-$VARIANT && property:ro.boot.bootloader=$t" >> "$WORK_DIR/vendor/etc/init/tee_blobs.rc"
     echo "mount none /vendor/tee_$t /vendor/tee bind" >> "$WORK_DIR/vendor/etc/init/tee_blobs.rc"
 
-    if ! grep -q "vendor/$t" "$WORK_DIR/configs/file_context-vendor"; then
+    if ! grep -q "vendor/tee_$t" "$WORK_DIR/configs/file_context-vendor"; then
       {
         echo "/vendor/tee_$t u:object_r:tee_file:s0"
         echo "/vendor/tee_$t/00000000-0000-0000-0000-000000010081 u:object_r:tee_file:s0"
@@ -79,7 +84,6 @@ if [ "$UNICA" = "1" ]; then
         echo "/vendor/tee_$t/00000000-0000-0000-0000-4d7073617574 u:object_r:tee_file:s0"
         echo "/vendor/tee_$t/00000000-0000-0000-0000-505256544545 u:object_r:tee_file:s0"
         echo "/vendor/tee_$t/00000000-0000-0000-0000-5345435f4652 u:object_r:tee_file:s0"
-        echo "/vendor/tee_$t/00000000-0000-0000-0000-53454d655345 u:object_r:tee_file:s0"
         echo "/vendor/tee_$t/00000000-0000-0000-0000-54412d48444d u:object_r:tee_file:s0"
         echo "/vendor/tee_$t/00000000-0000-0000-0000-54496473706c u:object_r:tee_file:s0"
         echo "/vendor/tee_$t/00000000-0000-0000-0000-544974684c6c u:object_r:tee_file:s0"
@@ -101,7 +105,17 @@ if [ "$UNICA" = "1" ]; then
       } >> "$WORK_DIR/configs/file_context-vendor"
     fi
 
-    if ! grep -q "vendor/$t" "$WORK_DIR/configs/fs_config-vendor"; then
+    if [ "$VARIANT" = "A536B" ]; then
+      if ! grep -q "vendor/tee_$t/00000000-0000-0000-0000-53454d655345" "$WORK_DIR/configs/file_context-vendor"; then
+        echo "/vendor/tee_$t/00000000-0000-0000-0000-53454d655345 u:object_r:tee_file:s0" >> "$WORK_DIR/configs/file_context-vendor"
+      fi
+    elif [ "$VARIANT" = "A536E" ]; then
+      if ! grep -q "vendor/tee_$t/00000000-0000-0000-0000-544545535355" "$WORK_DIR/configs/file_context-vendor"; then
+        echo "/vendor/tee_$t/00000000-0000-0000-0000-544545535355 u:object_r:tee_file:s0" >> "$WORK_DIR/configs/file_context-vendor"
+      fi
+    fi
+
+    if ! grep -q "vendor/tee_$t" "$WORK_DIR/configs/fs_config-vendor"; then
       {
         echo "vendor/tee_$t 0 2000 755 capabilities=0x0"
         echo "vendor/tee_$t/00000000-0000-0000-0000-000000010081 0 0 644 capabilities=0x0"
@@ -123,7 +137,6 @@ if [ "$UNICA" = "1" ]; then
         echo "vendor/tee_$t/00000000-0000-0000-0000-4d7073617574 0 0 644 capabilities=0x0"
         echo "vendor/tee_$t/00000000-0000-0000-0000-505256544545 0 0 644 capabilities=0x0"
         echo "vendor/tee_$t/00000000-0000-0000-0000-5345435f4652 0 0 644 capabilities=0x0"
-        echo "vendor/tee_$t/00000000-0000-0000-0000-53454d655345 0 0 644 capabilities=0x0"
         echo "vendor/tee_$t/00000000-0000-0000-0000-54412d48444d 0 0 644 capabilities=0x0"
         echo "vendor/tee_$t/00000000-0000-0000-0000-54496473706c 0 0 644 capabilities=0x0"
         echo "vendor/tee_$t/00000000-0000-0000-0000-544974684c6c 0 0 644 capabilities=0x0"
@@ -143,6 +156,16 @@ if [ "$UNICA" = "1" ]; then
         echo "vendor/tee_$t/tui/resolution_common 0 2000 755 capabilities=0x0"
         echo "vendor/tee_$t/tui/resolution_common/ID00000100 0 0 644 capabilities=0x0"
       } >> "$WORK_DIR/configs/fs_config-vendor"
+    fi
+
+    if [ "$VARIANT" = "A536B" ]; then
+      if ! grep -q "vendor/tee_$t/00000000-0000-0000-0000-53454d655345" "$WORK_DIR/configs/fs_config-vendor"; then
+        echo "vendor/tee_$t/00000000-0000-0000-0000-53454d655345 0 0 644 capabilities=0x0" >> "$WORK_DIR/configs/fs_config-vendor"
+      fi
+    elif [ "$VARIANT" = "A536E" ]; then
+      if ! grep -q "vendor/tee_$t/00000000-0000-0000-0000-544545535355" "$WORK_DIR/configs/fs_config-vendor"; then
+        echo "vendor/tee_$t/00000000-0000-0000-0000-544545535355 0 0 644 capabilities=0x0" >> "$WORK_DIR/configs/fs_config-vendor"
+      fi 
     fi
   done
 
